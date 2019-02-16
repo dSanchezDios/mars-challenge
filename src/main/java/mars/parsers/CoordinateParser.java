7ae866e6f03e84b8b6a108c0795a424deafc2069
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-16 19:38:28 +0100

    Reformat

--- src/main/java/mars/parsers/CoordinateParser.java
+++ src/main/java/mars/parsers/CoordinateParser.java
@@ -5,12 +5,22 @@ import mars.model.Coordinate;
 import static java.lang.Integer.parseInt;
 
 public class CoordinateParser {
+
 	Coordinate parse(String input) {
+		checkInput(input);
+
+		return new Coordinate(getNumber(input));
+	}
+
+	private void checkInput(String input) {
 		if (input == null || input.trim().isEmpty()) {
 			throw new IllegalArgumentException();
 		}
+	}
+
+	private int getNumber(String input) {
 		try {
-			return new Coordinate(parseInt(input));
+			return parseInt(input);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException();
 		}
