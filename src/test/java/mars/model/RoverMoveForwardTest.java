c90bc6d05fae995d66a3202bad9ec4765f60c1c6
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-17 12:18:55 +0100

    moved obstacles tests to another class

--- src/test/java/mars/model/RoverMoveForwardTest.java
+++ src/test/java/mars/model/RoverMoveForwardTest.java
@@ -4,26 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 
-import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static mars.model.Instruction.f;
 import static mars.model.Orientation.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RoverMoveForwardTest {
-
-	@Test
-	void shouldFailWhenThereIsAnObstacle() {
-		final var instructionList = asList(f, f, f, f);
-		final HashSet<Dimension> obstacles = new HashSet<>();
-		obstacles.add(new Dimension(new Coordinate(0), new Coordinate(2)));
-		final var map = new Map(new Dimension(new Coordinate(5), new Coordinate(6)), obstacles);
-		final var position = new Position(new Dimension(new Coordinate(0), new Coordinate(0)), N);
-		final var rover = new Rover(map, instructionList, position);
-
-		assertThrows(IllegalArgumentException.class, rover::executeInstructions);
-	}
 
 	@Test
 	void shouldReturnExpectedPositionWhenMoveToNorthNoObstacles() {
