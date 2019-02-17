ba7d55d274b71ff8ba8380127b733e790b16c968
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-17 16:29:09 +0100

    added message when exception

--- src/main/java/mars/parsers/CoordinateParser.java
+++ src/main/java/mars/parsers/CoordinateParser.java
@@ -8,6 +8,8 @@ public class CoordinateParser {
 
 	private static final IllegalArgumentException NOT_A_NUMBER =
 			new IllegalArgumentException("Input is not a coordinate valid.");
+	private static final IllegalArgumentException NULL_OR_EMPTY =
+			new IllegalArgumentException("Coordinate cant be null or empty.");
 
 	Coordinate parse(String input) {
 		checkInput(input);
@@ -17,7 +19,7 @@ public class CoordinateParser {
 
 	private void checkInput(String input) {
 		if (input == null || input.trim().isEmpty()) {
-			throw new IllegalArgumentException();
+			throw NULL_OR_EMPTY;
 		}
 	}
 
