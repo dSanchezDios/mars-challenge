33bf1d57b2eaff8f20649c24854912612493c74a
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-17 19:47:04 +0100

    Implemented interactive mode function

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,21 +1,69 @@
-import mars.RoverUtils;
+import mars.model.Instruction;
+import mars.model.Position;
 
 import java.util.Scanner;
 
+import static mars.RoverUtils.createRover;
+import static mars.RoverUtils.launch;
+
 public class MarsRover {
+
+	private static final IllegalArgumentException WRONG_MODE = new IllegalArgumentException("Execute in array mode with -a or interactive mode with -i.");
 
 	public static void main(String[] args) {
 
-		var map = getMapInput();
-		var rover = getRoverPositionInput();
-		var obstacles = getObstaclesInput();
-		var instructions = getInstructionsInput();
+		checkArguments(args);
+		final var mode = args[0];
 
-		printInputs(map, rover, instructions);
+		final var mapInput = getMapInput();
+		final var positionInput = getRoverPositionInput();
+		final var obstaclesInput = getObstaclesInput();
 
-		System.out.println("Rover output:");
-		System.out.println(RoverUtils.launch(map, obstacles, instructions, rover));
+		if (mode.equals("-a")) {
+			final var instructionsInput = getInstructionsInput();
+			instructionsMode(mapInput, obstaclesInput, instructionsInput, positionInput);
+		} else if (mode.equals("-i")) {
+			interactiveMode(mapInput, obstaclesInput, positionInput);
+		} else {
+			throw WRONG_MODE;
+		}
 	}
+
+	private static void instructionsMode(String map, String obstacles, String instructions, String rover) {
+		printInputs(map, rover, instructions);
+		System.out.println("Rover output:");
+		System.out.println(launch(map, obstacles, instructions, rover));
+	}
+
+	private static void interactiveMode(String mapInput, String obstaclesInput, String roverInput) {
+		printInstructions();
+		final var rover = createRover(mapInput, obstaclesInput, null, roverInput);
+		final var scanner = new Scanner(System.in);
+		var instruction = scanner.nextLine();
+		printRoverPosition(rover.executeInstructions(Instruction.valueOf(instruction)));
+
+		do {
+			printInstructions();
+			printRoverPosition(rover.executeInstructions(Instruction.valueOf(instruction)));
+			instruction = scanner.nextLine();
+		} while (!instruction.equals("X"));
+	}
+
+	private static void checkArguments(String[] args) {
+		if (args == null || args.length != 1 || !(args[0].equals("-a") || args[0].equals("-i"))) {
+			throw WRONG_MODE;
+		}
+	}
+
+	private static void printInstructions() {
+		System.out.println("Introduce an instruction: f move forward, b move backward, r right or l left.");
+		System.out.println("Press X to exit.");
+	}
+
+	private static void printRoverPosition(Position position) {
+		System.out.println("Rover position: " + position);
+	}
+
 
 	private static String getRoverPositionInput() {
 		System.out.println("Insert rover position ex \"0 0 N\"");
