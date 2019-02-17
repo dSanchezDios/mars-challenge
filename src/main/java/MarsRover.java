19c7f535e5d7baf76766b9a9fbc5b71c6fc1084c
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-17 11:28:37 +0100

    refactor in main class

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -5,32 +5,33 @@ import java.util.Scanner;
 public class MarsRover {
 
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
+		var reader = new Scanner(System.in);
 		System.out.println("Insert map size: ex \"5 6\"");
-		String map = reader.nextLine();
+		var map = reader.nextLine();
 
 		System.out.println("Insert rover position: ex \"0 0 N\"");
-		String rover = reader.nextLine();
+		var rover = reader.nextLine();
 
 		//TODO get obstacles
 
+		System.out.println("Insert list of instructions: ex: \"lbrf\"");
 		System.out.println("Instructions forward, b = backward, l = turn left, r = turn right):");
-		System.out.println("Insert list of instructions: (ex: lbrf)");
-		String instructions = reader.nextLine();
+		var instructions = reader.nextLine();
 
 		printInputs(map, rover, instructions);
 
 		System.out.println(RoverUtils.launch(map, null, instructions, rover));
+		reader.close();
 	}
 
 	private static void printInputs(String map, String rover, String instructionsNumber) {
 		System.out.println("Inputs:");
-		System.out.println("Map:");
-		System.out.println(map);
-		System.out.println("Rover:");
-		System.out.println(rover);
-		System.out.println("Instructions:");
-		System.out.println(instructionsNumber);
+		System.out.println("Map: ");
+		System.out.print(map);
+		System.out.println("Rover: ");
+		System.out.print(rover);
+		System.out.println("Instructions: ");
+		System.out.print(instructionsNumber);
 	}
 
 }
